fce36d819eaf67190a68ceb26c1f0de1d0061785
Viktor Shvetcov <vikshv@yandex.ru>
2017-10-31 16:30:29 +0300

    add bind message event and log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    messages: Array<any>;
+
+    constructor() {
+        this.messages = [];
+    }
+
+    ngOnInit() {
+        console.log('OnInit', window['alphaopen']);
+    }
+
+    @HostListener('window:message', ['$event'])
+    onMessage(event) {
+        this.messages.push({
+            data: event.data,
+            origin: event.origin
+        });
+        
+        if (event.data === 'aoWidgetConnect') {
+            event.source.postMessage('connect', event.origin);
+        }
+    }
 }
